10f2bb181a017e0cf2790b38794b2fec335dc844
Ahmed El Sheikh <host12chat@gmail.com>
2025-04-03 23:40:57 +0200

    Commit changes
    Commit changes

--- system/action/action.php
+++ system/action/action.php
@@ -1,5 +1,5 @@
 <?php
-require('./../config_session.php');
+//require('./../config_session.php');
 
 // Function to handle actions
 function handleAction($action, $target) {
@@ -53,101 +53,124 @@ function kickStatus(){
     return 0;
 }
 
-
-// Handle action requests
-if (isset($_POST['take_action'], $_POST['target'])) {
-    $action = escape($_POST['take_action']);
-    $target = escape($_POST['target']);
-    echo handleAction($action, $target);
-    die();
+if ($f == 'action') {
+	if($s == 'check_kick') {
+		// Check if user is kicked
+		if(isset($_POST['check_kick'])){
+			echo kickStatus();
+			die();
+		}		
+	}
+	if($s == 'check_maintenance') {
+		// Check if maintenance mode is enabled
+		if (isset($_POST['check_maintenance'])) {
+			echo maintenanceStatus();
+			die();
+		}
+	}
+	if($s == 'take_action') {
+		// Handle action requests
+		if (isset($_POST['take_action'], $_POST['target'])) {
+				$action = escape($_POST['take_action']);
+				$target = escape($_POST['target']);
+				echo handleAction($action, $target);
+				die();
+		}
+	}
+	if($s == 'kick') {
+		// Handle kick requests
+		if (isset($_POST['kick'], $_POST['reason'], $_POST['delay'])) {
+			$target = escape($_POST['kick']);
+			$reason = escape($_POST['reason']);
+			$delay = escape($_POST['delay']);
+			echo kickAccount($target, $delay, $reason);
+			die();
+		}
+	}
+	if($s == 'room_mute') {
+		// Handle room mute requests
+		if (isset($_POST['room_mute'], $_POST['reason'], $_POST['delay'])) {
+			$target = escape($_POST['room_mute']);
+			$reason = escape($_POST['reason']);
+			$delay = escape($_POST['delay']);
+			echo muteRoom($target, $delay, $reason);
+			die();
+		}		
+	}
+	if($s == 'room_block') {
+		// Handle room block requests
+		if(isset($_POST['room_block'], $_POST['reason'], $_POST['delay'])){
+			$target = escape($_POST['room_block'], true);
+			$reason = escape($_POST['reason']);
+			$delay = escape($_POST['delay'], true);
+			echo blockRoom($target, $delay, $reason);
+			die();
+		}	
+	}
+	if($s == 'mute') {
+		// Handle mute requests
+		if (isset($_POST['mute'], $_POST['reason'], $_POST['delay'])) {
+			$target = escape($_POST['mute']);
+			$reason = escape($_POST['reason']);
+			$delay = escape($_POST['delay']);
+			echo muteAccount($target, $delay, $reason);
+			die();
+		}		
+	}
+	if($s == 'main_mute') {
+		// Handle main mute requests
+		if (isset($_POST['main_mute'], $_POST['reason'], $_POST['delay'])) {
+			$target = escape($_POST['main_mute']);
+			$reason = escape($_POST['reason']);
+			$delay = escape($_POST['delay']);
+			echo muteAccountMain($target, $delay, $reason);
+			die();
+		}	
+	}
+	if($s == 'private_mute') {
+		if (isset($_POST['private_mute'], $_POST['reason'], $_POST['delay'])) {
+			$target = escape($_POST['private_mute']);
+			$reason = escape($_POST['reason']);
+			$delay = escape($_POST['delay']);
+			echo muteAccountPrivate($target, $delay, $reason);
+			die();
+		}		
+	}
+	if($s == 'ghost') {	
+		// Handle ghost requests
+		if(isset($_POST['ghost'], $_POST['reason'], $_POST['delay'])){
+			$target = escape($_POST['ghost'], true);
+			$reason = escape($_POST['reason']);
+			$delay = escape($_POST['delay'], true);
+			echo ghostAccount($target, $delay, $reason);
+			die();
+		}	
+	}
+	if($s == 'ban') {	
+		// Handle ban requests
+		if (isset($_POST['ban'], $_POST['reason'])) {
+			$target = escape($_POST['ban']);
+			$reason = escape($_POST['reason']);
+			echo banAccount($target, $reason);
+			die();
+		}	
+	}
+	if($s == 'warn') {
+		if(isset($_POST['warn'], $_POST['reason'])){
+			$target = escape($_POST['warn'], true);
+			$reason = escape($_POST['reason']);
+			echo warnAccount($target, $reason);
+			die();
+		}		
+	}
+	if($s == 'remove_room_staff') {
+		// Remove room staff
+		if (isset($_POST['remove_room_staff'], $_POST['target'])) {
+			$target = escape($_POST['target']);
+			echo removeRoomStaff($target);
+			die();
+		}
+	}	
 }
-
-// Check if user is kicked
-if(isset($_POST['check_kick'])){
-	echo kickStatus();
-	die();
-}
-
-// Check if maintenance mode is enabled
-if (isset($_POST['check_maintenance'])) {
-    echo maintenanceStatus();
-    die();
-}
-
-// Handle kick requests
-if (isset($_POST['kick'], $_POST['reason'], $_POST['delay'])) {
-    $target = escape($_POST['kick']);
-    $reason = escape($_POST['reason']);
-    $delay = escape($_POST['delay']);
-    echo kickAccount($target, $delay, $reason);
-    die();
-}
-// Handle kick requests
-if (isset($_POST['room_mute'], $_POST['reason'], $_POST['delay'])) {
-    $target = escape($_POST['room_mute']);
-    $reason = escape($_POST['reason']);
-    $delay = escape($_POST['delay']);
-    echo muteRoom($target, $delay, $reason);
-    die();
-}
-// Handle room block requests
-if(isset($_POST['room_block'], $_POST['reason'], $_POST['delay'])){
-	$target = escape($_POST['room_block'], true);
-	$reason = escape($_POST['reason']);
-	$delay = escape($_POST['delay'], true);
-	echo blockRoom($target, $delay, $reason);
-	die();
-}
-// Handle mute requests
-if (isset($_POST['mute'], $_POST['reason'], $_POST['delay'])) {
-    $target = escape($_POST['mute']);
-    $reason = escape($_POST['reason']);
-    $delay = escape($_POST['delay']);
-    echo muteAccount($target, $delay, $reason);
-    die();
-}
-// Handle main mute requests
-if (isset($_POST['main_mute'], $_POST['reason'], $_POST['delay'])) {
-    $target = escape($_POST['main_mute']);
-    $reason = escape($_POST['reason']);
-    $delay = escape($_POST['delay']);
-    echo muteAccountMain($target, $delay, $reason);
-    die();
-}
-if (isset($_POST['private_mute'], $_POST['reason'], $_POST['delay'])) {
-    $target = escape($_POST['private_mute']);
-    $reason = escape($_POST['reason']);
-    $delay = escape($_POST['delay']);
-    echo muteAccountPrivate($target, $delay, $reason);
-    die();
-}
-// Handle ghost requests
-if(isset($_POST['ghost'], $_POST['reason'], $_POST['delay'])){
-	$target = escape($_POST['ghost'], true);
-	$reason = escape($_POST['reason']);
-	$delay = escape($_POST['delay'], true);
-	echo ghostAccount($target, $delay, $reason);
-	die();
-}
-// Handle ban requests
-if (isset($_POST['ban'], $_POST['reason'])) {
-    $target = escape($_POST['ban']);
-    $reason = escape($_POST['reason']);
-    echo banAccount($target, $reason);
-    die();
-}
-if(isset($_POST['warn'], $_POST['reason'])){
-	$target = escape($_POST['warn'], true);
-	$reason = escape($_POST['reason']);
-	echo warnAccount($target, $reason);
-	die();
-}
-// Remove room staff
-if (isset($_POST['remove_room_staff'], $_POST['target'])) {
-    $target = escape($_POST['target']);
-    echo removeRoomStaff($target);
-    die();
-}
-
 
 ?>

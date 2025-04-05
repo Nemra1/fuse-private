e8d8ef308445ae605115c6d06d90c9a6da382741
Ahmed El Sheikh <host12chat@gmail.com>
2025-04-05 02:40:43 +0200

    Commit changes
    Commit changes

--- system/action/one_signal.php
+++ system/action/one_signal.php
@@ -1,11 +1,12 @@
 <?php
-$array_data = array();
+$res = array();
 if ($f == 'one_signal') {
          if ($s == 'mass_notifications') {
             $message = $_POST['mass_message'];
-             $array_data['content']= sendNotificationToAll($message);
+             $res['content']= sendNotificationToAll($message);
+             $res['status']= 200;
              header("Content-type: application/json");
-             echo json_encode($array_data);
+             echo json_encode($res);
              exit();             
          
     }

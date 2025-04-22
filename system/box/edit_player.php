472c08d11ed0ee354bcb2cfc7028865bf621e6bf
Ahmed El Sheikh <host12chat@gmail.com>
2025-04-22 04:46:44 +0200

    Commit changes

--- system/box/edit_player.php
+++ system/box/edit_player.php
@@ -1,33 +1,18 @@
 <?php
 require_once('../config_session.php');
-if (!isset($_POST['edit_player'])) {
-    // Return an error response instead of die()
-    echo json_encode(['status' => 'error', 'message' => 'Player ID is missing.']);
-    exit();
+
+if(!isset($_POST['edit_player'])){
+	die();
 }
-// Sanitize the input using escape() function
 $id = escape($_POST['edit_player']);
-// Use a prepared statement to prevent SQL injection
-$stmt = $mysqli->prepare("SELECT * FROM boom_radio_stream WHERE id = ?");
-$stmt->bind_param("i", $id); // Bind the ID as an integer
-$stmt->execute();
-$result = $stmt->get_result();
-// Check if the player exists
-if ($result->num_rows < 1) {
-    echo json_encode(['status' => 'error', 'message' => 'Player not found.']);
-    exit();
-} else {
-    // Fetch the player data securely
-    $player = $result->fetch_assoc();
-    // Optionally, you can return player data as a JSON response if it's an API
-    echo json_encode(['status' => 'success', 'player' => $player]);
-    exit();
+$get_player = $mysqli->query("SELECT * FROM boom_radio_stream WHERE id = '$id'");
+if($get_player->num_rows < 1){
+	die();
 }
-
-// Close the prepared statement
-$stmt->close();
+else {
+	$player = $get_player->fetch_assoc();
+}
 ?>
-
 <div class="pad_box">
 	<div class="setting_element ">
 		<p class="label"><?php echo $lang['stream_alias']; ?></p>
@@ -37,6 +22,6 @@ $stmt->close();
 		<p class="label"><?php echo $lang['stream_url']; ?></p>
 		<input id="new_player_url" class="full_input" value="<?php echo $player['stream_url']; ?>"/>
 	</div>
-	<button onclick="savePlayer(<?php echo $player['id']; ?>);" type="button" class="reg_button theme_btn tmargin10"><i class="ri-save-line"></i> <?php echo $lang['save']; ?></button>
+	<button onclick="savePlayer(<?php echo $player['id']; ?>);" type="button" class="reg_button theme_btn tmargin10"><i class="fa fa-save"></i> <?php echo $lang['save']; ?></button>
 	<button type="button" class="reg_button cancel_modal default_btn"><?php echo $lang['cancel']; ?></button>
 </div>
--- system/box/show_private_report.php
+++ system/box/show_private_report.php
@@ -2,68 +2,48 @@
 require('../config_session.php');
 
 $show_report = '';
-
-if (!canManageReport()) {
-    echo json_encode(['status' => 'error', 'message' => 'Permission denied.']);
-    exit();
+if(!canManageReport()){
+	die();
 }
-
-if (isset($_POST['private_report'])) {
-    $id = escape($_POST['private_report']);  // Sanitize the report ID input
-    // Fetch report info securely using prepared statements
-    $stmt_report = $mysqli->prepare("SELECT * FROM boom_report WHERE report_id = ? AND report_type = 3");
-    $stmt_report->bind_param("i", $id);
-    $stmt_report->execute();
-    $result_report = $stmt_report->get_result();
-    if ($result_report->num_rows < 1) {
-        echo json_encode(['status' => 'error', 'message' => 'Report not found or has been deleted.']);
-        exit();
-    }
-    $report = $result_report->fetch_assoc();
-    // Fetch private log data securely
-    $stmt_privlog = $mysqli->prepare("
-        SELECT log.*, boom_users.user_id, boom_users.user_name, boom_users.user_color, boom_users.user_tumb, boom_users.user_bot 
-        FROM ( 
-            SELECT * FROM boom_private 
-            WHERE (hunter = ? AND target = ?) OR (hunter = ? AND target = ?) 
-            ORDER BY id DESC LIMIT ?
-        ) AS log 
-        LEFT JOIN boom_users ON log.hunter = boom_users.user_id 
-        ORDER BY log.time DESC
-    ");
-    // Securely bind the parameters for the private log query
-    $stmt_privlog->bind_param("iiiii", $report['report_user'], $report['report_target'], $report['report_target'], $report['report_user'], $cody['report_history']);
-    $stmt_privlog->execute();
-    $result_privlog = $stmt_privlog->get_result();
-    if ($result_privlog->num_rows > 0) {
-        while ($log = $result_privlog->fetch_assoc()) {
-            $show_report .= privateLog($log, $report['report_user']);
-        }
-    } else {
-        // Delete report if no private logs found and notify staff
-        $stmt_delete = $mysqli->prepare("DELETE FROM boom_report WHERE report_id = ? AND report_type = 3");
-        $stmt_delete->bind_param("i", $id);
-        $stmt_delete->execute();
-        updateStaffNotify();
-        echo json_encode(['status' => 'success', 'message' => 'No logs found, report deleted.']);
-        exit();
-    }
-    echo json_encode(['status' => 'success', 'report' => $show_report]);
-    exit();
-} else {
-    echo json_encode(['status' => 'error', 'message' => 'Private report ID is missing.']);
-    exit();
+if(isset($_POST['private_report'])){
+	$id = escape($_POST['private_report']);
+	$report = reportInfo($id);
+	if(empty($report)){
+		echo 1;
+		die();
+	}
+	$privlog = $mysqli->query("
+		SELECT 
+		log.*, boom_users.user_id, boom_users.user_name, boom_users.user_color, boom_users.user_tumb, boom_users.user_bot 
+		FROM ( SELECT * FROM `boom_private` WHERE  `hunter` = '{$report['report_user']}' AND `target` = '{$report['report_target']}'  OR `hunter` = '{$report['report_target']}' AND `target` = '{$report['report_user']}' ORDER BY `id` DESC LIMIT {$cody['report_history']}) AS log 
+		LEFT JOIN boom_users
+		ON log.hunter = boom_users.user_id
+		ORDER BY `time` DESC
+	");
+	if($privlog->num_rows > 0){
+		while($log = $privlog->fetch_assoc()){
+			$show_report .= privateLog($log, $report['report_user']);
+		}
+	}
+	else {
+		$mysqli->query("DELETE FROM boom_report WHERE report_id = '$id' AND report_type = 3");
+		updateStaffNotify();
+		echo 1;
+		die();
+	}
+}
+else {
+	die();
 }
 ?>
-
 <div>
-    <div id="preport_box" class="background_box box_height300 pad20">
-        <?php echo $show_report; ?>
-    </div>
-    <div class="btable pad20" id="report_control">
-        <div class="bcell report_action">
-            <button onclick="removeReport(3,<?php echo $report['report_id']; ?>, <?php echo $report['report_target']; ?>);" class="reg_button delete_btn"><?php echo $lang['do_action']; ?></button>
-            <button onclick="unsetReport(<?php echo $report['report_id']; ?>, 3);" class="unset_report reg_button default_btn"><?php echo $lang['action_none']; ?></button>
-        </div>
-    </div>
+	<div id="preport_box" class="background_box box_height300 pad20">
+		<?php echo $show_report; ?>
+	</div>
+	<div class="btable pad20" id="report_control">
+		<div class="bcell report_action">
+			<button onclick="removeReport(3,<?php echo $report['report_id']; ?>, <?php echo $report['report_target']; ?>);" class="reg_button delete_btn"><?php echo $lang['do_action']; ?></button>
+			<button onclick="unsetReport(<?php echo $report['report_id']; ?>, 3);" class="unset_report reg_button default_btn"><?php echo $lang['action_none']; ?></button>
+		</div>
+	</div>
 </div>
